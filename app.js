519e94d425fb99d0497cbb7ada6316dab252abb5
ALEX HENRIQUE DA CRUZ GOMES <25132769@CFP127ALUNO.LOCAL>
2025-09-09 16:57:27 -0300

    terminado o JS agora só falta a integração do css

---
+++ app.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const campoPesquisa = document.getElementById('pesquisa')
+const btnPesquisa = document.getElementById('btn-cachorro')
+
+// async function buscarImagens (raca) {
+//     const url = `https://dog.ceo/api/breed/${raca}/images`
+//     const response = await fetch(url)
+//     const imagens = await response.json()
+//     console.log(imagens.message)
+//     return imagens.message
+// }
+
+async function criarImagemCachorro () {
+    const raca = campoPesquisa.value
+    const url = `https://dog.ceo/api/breed/${raca}/images`
+    const response = await fetch(url)
+    const imagens = await response.json()
+    console.log(imagens.message)
+    const diva = document.getElementById('imagem-cachorro')
+    diva.textContent = ""
+
+    imagens.message.forEach(url => {
+
+        const diva = document.getElementById('imagem-cachorro')
+        const img = document.createElement('img')
+
+        img.src = url
+
+        diva.appendChild(img)
+    });
+
+}
+
+btnPesquisa.addEventListener('click', criarImagemCachorro)
+
